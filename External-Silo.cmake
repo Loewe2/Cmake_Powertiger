a4598e62a78864785c3ad900d6457b54505c1d93
Alexander Van Craen <vancraar@ipvs.uni-stuttgart.de>
2020-02-20 14:35:20 +0100

    More BOOST dependancies for HPX and silance Silo Download

--- External-Silo.cmake
+++ External-Silo.cmake
@@ -6,6 +6,7 @@ ExternalProject_Add(
     Silo
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/silo
         URL http://phys.lsu.edu/~dmarcel/silo-${SILO_TARGET_VERSION}.tar.gz
+        DOWNLOAD_NO_PROGRESS TRUE
         # URL_MD5 f4096c4583947b0eb103c8539f1623a3 TODO
         # TODO: sed raus!! (saubere Lib Pfade) sed -i "'s/-lhdf5/$hdf5_lib\/libhdf5.a -ldl/g'" configure &&
         PATCH_COMMAND  autoreconf -ifv && ${PROJECT_SOURCE_DIR}/sed.sh
